f32ca4775d0f9f433553ab7ded4e297cc4aa746c
Stephan-de-Jong <152481591+Stephan-de-Jong@users.noreply.github.com>
2025-11-30 23:06:01 +0100

    Added device ID

---
+++ drivers/mini_zigbee_switch_SJO/driver.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+const { ZigBeeDriver } = require('homey-zigbeedriver');
+
+class mini_zigbee_switch_SJO_driver extends ZigBeeDriver {
+
+}
+
+module.exports = mini_zigbee_switch_SJO_driver;
